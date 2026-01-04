631d0515644f810b3767bfca2335662314545745
Sakibwahid <sakibruns@gmail.com>
2026-01-03 18:35:12 -0800

    vercel check

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -1,23 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC1-tCmJHa2pkfmaWrC45ToaTzIDZkhWnk",
+  authDomain: "auction19-f259d.firebaseapp.com",
+  projectId: "auction19-f259d",
+  storageBucket: "auction19-f259d.firebasestorage.app",
+  messagingSenderId: "652902959670",
+  appId: "1:652902959670:web:319342e952929a53872697",
+  measurementId: "G-BVPRTNZF4F"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);export const auth = getAuth(app);
 
-// Initialize services
-export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
