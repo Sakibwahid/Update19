37d3f76f4ded1fce6df19c0a72e364aca0d5dc89
Sakibwahid <sakibruns@gmail.com>
2026-01-03 19:44:49 -0800

    rollback

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import UserDashboard from "./pages/UserDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import Home from "./pages/Home";
 import PlayerDetails from "./components/player/PlayerDetails";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="bg-blue-900 min-h-screen text-black">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/user" element={<UserDashboard />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/player-details" element={<PlayerDetails />} />
-        </Routes>
-      </div>
+      <Router>
+        <div className="bg-blue-900 min-h-screen text-black">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/user" element={<UserDashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/player-details" element={<PlayerDetails />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
 
 export default App;
+
 
