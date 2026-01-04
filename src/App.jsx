1672ffe4230fca648b048faf837794f72eee0463
Sakibwahid <sakibruns@gmail.com>
2026-01-03 19:29:26 -0800

    vercel route fiix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-// Import from correct path (no 'admin' folder)
-import TestAuth from './pages/TestAuth';  // ✅ Fixed
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import UserDashboard from './pages/UserDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import Home from './pages/Home';
-import PlayerDetails from './components/player/PlayerDetails';  
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import UserDashboard from "./pages/UserDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home";
+import PlayerDetails from "./components/player/PlayerDetails";
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <div className="bg-blue-900 min-h-screen text-black">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/user" element={<UserDashboard />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/player-details" element={<PlayerDetails />} />
-
-          </Routes>
-        </div>
-      </Router>
+      <div className="bg-blue-900 min-h-screen text-black">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<UserDashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/player-details" element={<PlayerDetails />} />
+        </Routes>
+      </div>
     </AuthProvider>
   );
 }
 
 export default App;
+
